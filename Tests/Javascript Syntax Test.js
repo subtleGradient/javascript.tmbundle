37132a7244906e0d188f351e80775b48d4e7aa81
subtlegradient <subtlegradient@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-07-05 23:42:13 +0000

    More and more syntax tests
    git-svn-id: http://macromates.com/svn/Bundles/trunk/Review/Bundles/JavaScript.tmbundle@10169 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tests/Javascript Syntax Test.js
+++ Tests/Javascript Syntax Test.js
@@ -92,6 +92,8 @@ funkName(
 // =========
 // = REGEX =
 // =========
+// WARNING: regular expressions should be preceded by a left parenthesis, assignment, colon, or comma
+
 var regexName = /regex/
 
 "stringName".match(/regex/)
@@ -190,3 +192,63 @@ x <<= 5
 x >>= 5
 x >>>= 5
 
+
+// ===========
+// = Strings =
+// ===========
+
+var stringName = "";
+var stringName = '';
+
+var stringName0 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+var stringName1 = "Lorem ipsum dolor sit amet, consectetur adipisicing e" + 
+"lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." + 
+" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis" + 
+"i ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehen" + 
+"derit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. E" + 
+"xcepteur sint occaecat cupidatat non proident, sunt in culpa qui offici" + 
+"a deserunt mollit anim id est laborum.";
+
+// Allow escaped newline
+var stringName2 = "Lorem ipsum dolor sit amet, consectetur adipisicing e\
+SHOULD be scoped as string"
+var stringName2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing e\
+SHOULD be scoped as string'
+// First newline should be scoped as constant.character.escape.newline.js
+
+// End string at unescaped newline
+// SYNTAX ERROR: unterminated string literal
+var stringName3 = "Lorem ipsum dolor sit amet, consectetur adipisicing e
+Should NOT be scoped as string"
+var stringName3 = 'Lorem ipsum dolor sit amet, consectetur adipisicing e
+Should NOT be scoped as string'
+// First newline SHOULD be scoped as invalid
+
+var stringName4 = "Lorem ipsum dolor sit amet</script>";
+"</script>" SHOULD be scoped as string & invalid.deprecated
+var stringName5 = "Lorem ipsum dolor sit amet</script>
+"</script>" Should NOT be scoped as string
+
+// WARNING: regular expressions should be preceded by a left parenthesis, assignment, colon, or comma
+
+ {unquotedString: null}
+ {'singleQuotedString': null}// SYNTAX ERROR: invalid label
+ {"doubleQuotedString": null}// SYNTAX ERROR: invalid label
+({'singleQuotedString': null})
+({"doubleQuotedString": null})
+
+switch (something){
+	case "stringName" : 
+		break; 
+	case unquotedString : 
+		break; 
+	case /regex/ : 
+		break; 
+	case 999.999 : 
+		break; 
+	case funkName() : 
+		break; 
+	default : 
+		// do something
+} 
