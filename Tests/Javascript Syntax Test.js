e696cf88a38293f8a09118ff755e048baadf1707
Thomas Aylott <oblivious+git@subtlegradient.com>
2008-07-28 10:49:24 -0400

    Tests for labels and new new

--- Tests/Javascript Syntax Test.js
+++ Tests/Javascript Syntax Test.js
@@ -863,3 +863,27 @@ const
 export
 import
 
+// Labels
+checkiandj:
+while (i < 4) {
+   document.write(i + "<br>");
+   i += 1;
+
+   checkj:
+   while (j > 4) {
+      document.write(j + "<br>");
+      j -= 1;
+      if ((j % 2) == 0)
+         continue checkj;
+      document.write(j + " is odd.<br>");
+   }
+   document.write("i = " + i + "<br>");
+   document.write("j = " + j + "<br>");
+}
+
+// Unorganized
+
+new new Function();
+new (function(){})();
+new(function(){})();
+
